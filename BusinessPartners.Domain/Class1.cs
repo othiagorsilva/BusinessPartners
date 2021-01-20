91f39f596bc939bac28347c085a982a630ec2db5
euthiagorsilva@gmail.com <euthiagorsilva@gmail.com>
2021-01-20 16:12:59 -0300

    Adicionando os projetos

---
+++ BusinessPartners.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BusinessPartners.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ BusinessPartners.InfraData/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BusinessPartners.InfraData
+{
+    public class Class1
+    {
+    }
+}
